990130b3d59261a65338ee3df756752a50e5c400
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2024-05-24 18:13:51 +0530

    board layout

---
+++ src/components/Board.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Board.css"
+export const Board = () => {
+  return (
+    <div className='board'>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+        <div className="btn"></div>
+    </div>
+  )
+}
